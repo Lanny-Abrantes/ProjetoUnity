4fbbda01d87f7ff4433c46756f94423ef8f0889c
Eryslânia Abrantes <lanny.abrantees@gmail.com>
2020-01-06 03:00:27 -0300

    Add files via upload

---
+++ LabirintoProject/Assets/Scripts/ColetarCanudo.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColetarCanudo : MonoBehaviour
+{
+    private int pontos;
+    public TextMesh pontuacao;
+	
+	public GameObject textoFinal;
+	public bool terminouJogo;
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        pontos = 0;
+		
+		textoFinal.SetActive(false);
+		terminouJogo = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pontuacao.text = "SCORE: " + pontos;
+		
+    }
+
+    void OnCollisionEnter2D(Collision2D collision){
+        GameObject collider = collision.collider.gameObject;
+        if(collider.tag == "Canudo"){
+            GameObject.Destroy(collider);
+            pontos++;
+        }
+		if(pontos >= 10 && collider.tag == "passagem"){
+			GameObject.Destroy(collider);
+		}
+		if(collider.tag == "espinho"){
+			Restart();
+		}
+		if(collider.tag == "bau"){
+			Time.timeScale = 0;
+			fimDeJogo();
+		}
+		
+		
+    }
+
+    void OnTriggerEnter2d(Collider2D collider){
+        if(collider.tag == "Canudo"){
+            GameObject.Destroy(collider.gameObject);
+        }
+		if(pontos >= 10 && collider.tag == "passagem"){
+			GameObject.Destroy(collider.gameObject);
+		}
+		if(collider.tag == "espinho"){
+			Restart();
+		}		
+		if(collider.tag == "bau"){
+			Time.timeScale = 0;
+			fimDeJogo();	
+		}
+		
+    }
+	
+	void fimDeJogo(){
+		textoFinal.SetActive(true);
+		terminouJogo = true;
+	}
+	
+		
+	public void Exit(){
+		Time.timeScale = 1;
+		Application.Quit();
+	}
+	
+	public void Restart(){
+		Time.timeScale = 1;
+		Application.LoadLevel("Cena1");
+	}
+}
---
+++ LabirintoProject/Assets/Scripts/personagem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class personagem : MonoBehaviour
+{
+    public float velocidade = 10.0f;
+    public float horizontal;
+    public float vertical;
+    public Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         rigidbody = GetComponent <Rigidbody2D>();    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        rigidbody.velocity = new Vector2(velocidade*horizontal,velocidade*vertical);
+    }
+}
